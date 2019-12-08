86521e22b879e33382ef578a459be6c4408930f5
wkylin.w <wkylin.w@gmail.com>
2019-12-08 10:18:01 +0800

    includes ...

---
+++ controllers/postController.js
@@ -0,0 +1,7 @@
+
+exports.viewCreateScreen = function (req, res) {
+  res.render('create-post',{
+    username:req.session.user.username,
+    avatar: req.session.user.avatar,
+  });
+};
--- router.js
+++ router.js
@@ -6,6 +6,9 @@ const router = express.Router();
 
 const userController = require('./controllers/userController')
 
+const postController = require('./controllers/postController.js')
+
+
 router.get('/', userController.home);
 
 router.post('/register', userController.register);
@@ -14,6 +17,7 @@ router.post('/login', userController.login);
 
 router.post('/logout', userController.logout);
 
+router.get('/create-post', userController.mustBeLoggedIn,postController.viewCreateScreen);
 
 
 
